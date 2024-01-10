507050d87c290fb5766e5de398dcfbe79ae842f7
Vayks123 <sergybibik1234@gmail.com>
2024-01-10 11:19:42 +0200

    add telescope plugins

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -28,4 +28,8 @@ require("lazy").setup({
 {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-path'},
 {'hrsh7th/cmp-cmdline'}, {'hrsh7th/nvim-cmp'},
 {"williamboman/mason.nvim"},
+{
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 })
